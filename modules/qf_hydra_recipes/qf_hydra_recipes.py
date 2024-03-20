46049a398017a716c940554c8032b46dc0df901a
Quentin Febvre <quentin.febvre@imt-atlantique.fr>
2024-03-20 12:18:17 +0000

    add pipeline module

---
+++ modules/qf_hydra_recipes/qf_hydra_recipes.py
@@ -0,0 +1,83 @@
+from pathlib import Path
+import s3fs
+import hydra_zen
+import numpy as np
+import ocn_tools._src.geoprocessing.gridding as ocngrid
+import pandas as pd
+import xarray as xr
+
+import qf_run_recipe
+
+pb = hydra_zen.make_custom_builds_fn(
+    zen_partial=True,
+)
+
+
+# GRIDDING
+
+b = hydra_zen.make_custom_builds_fn()
+params = dict(
+    input_path="data/prepared/inference_combined.nc",
+    grid=dict(
+        time=b(pd.date_range, start="2016-12-01", end="2018-01-31", freq="1D"),
+        lat=b(np.arange, start=32, stop=44, step=0.05),
+        lon=b(np.arange, start=-66, stop=-54, step=0.05),
+    ),
+    output_path="data/prepared/gridded.nc",
+)
+grid_cfg = dict(
+    _01_read_tracks=pb(
+        xr.open_dataset,
+        filename_or_obj="${...params.input_path}",
+    ),
+    _02_grid=pb(
+        ocngrid.coord_based_to_grid,
+        target_grid_ds=b(
+            xr.Dataset,
+            coords="${....params.grid}",
+        ),
+    ),
+    _03_write_grid=pb(xr.Dataset.to_netcdf, path="${...params.input_path}"),
+)
+
+qf_grid = qf_run_recipe.register_recipe(name="qf_grid", steps=grid_cfg, params=params)
+
+
+
+# CONCAT
+params = dict(
+    input_path="data/prepared/inference",
+    concat_dim="time",
+    output_path="data/prepared/concatenated.nc",
+)
+concat_cfg = dict(
+    _01_glob=pb(
+        Path.glob,
+        pattern="**/*.nc",
+    ),
+    _02_read=pb(map, xr.open_dataset),
+    _02_concat=pb(xr.concat, dim="${...params.concat_dim}"),
+    _03_write=pb(xr.Dataset.to_netcdf, path="${...params.output_path}"),
+)
+
+qf_concat = qf_run_recipe.register_recipe(
+    name="qf_concat", steps=concat_cfg, params=params, input="${.params.input_path}"
+)
+
+# Get from s3
+b = hydra_zen.make_custom_builds_fn()
+params = dict(
+    remote_path='???',
+    local_path='${.remote_path}',
+)
+
+grid_cfg = dict(
+    _01_get_s3fs=pb(
+        s3fs.S3FileSystem.get,
+        b(s3fs.S3FileSystem, anon=True, client_kwargs={"endpoint_url": 'https://s3.eu-central-1.wasabisys.com'}),
+        rpath='${..params.remote_path}',
+        lpath='${..params.remote_path}',
+    )
+)
+
+qf_get_s3 = qf_run_recipe.register_recipe(name="qf_s3_get", steps=qf_s3_get, params=params)
---
+++ modules/qf_hydra_recipes/qf_run_recipe.py
@@ -0,0 +1,84 @@
+import logging
+from typing import Any, Callable, Optional
+
+import hydra
+import hydra_zen
+import toolz
+from hydra.conf import HelpConf, HydraConf
+
+log = logging.getLogger(__name__)
+
+PIPELINE_DESC = "Apply sequence of configurable steps"
+
+
+def run(
+    inp: Optional[Any] = None,
+    steps: dict[str, Callable] = dict(),
+    params: Optional[dict] = dict(),
+):
+    log.info("Starting simple chaining with steps:")
+    log.info("\n".join(sorted(steps)))
+
+    if not steps:
+        log.info("No steps returning input")
+        return inp
+
+    sorted_steps = sorted(steps)
+    if inp is None:
+        log.info("No input given, computing it from first step")
+        inp = steps[sorted_steps.pop(0)]()
+    toolz.compose_left(*(steps[k] for k in sorted_steps))(inp)
+
+
+run.__doc__ = f"""
+Pipeline description:
+    {PIPELINE_DESC}
+
+Args:
+    inp: input to pipe through the steps, if none call first step without input
+    steps: dictionnary of key <-> callable that will be called in order of key
+
+Returns:
+    None
+"""
+
+# Wrap the function to accept the configuration as input
+zen_endpoint = hydra_zen.zen(run)
+
+
+# Store the config
+def register_recipe(name, steps, params=dict(), input=None):
+    steps_store = hydra_zen.store(group="ocb_mods/hydra_recipe", package="steps")
+    params_store = hydra_zen.store(
+        group="ocb_mods/hydra_recipe/params", package="_global_"
+    )
+    steps_store(steps, name=name, to_config=lambda x: x)
+    params_store(params, name=name, to_config=lambda x: x)
+    store = hydra_zen.store()
+
+    base_config = hydra_zen.builds(
+        run,
+        populate_full_signature=True,
+        zen_partial=True,
+    )
+
+    store(
+        hydra_zen.make_config(
+            dict(input=input),
+            bases=(base_config,),
+            hydra_defaults=["_self_", {"steps": name, "params": name}],
+        ),
+        name=name,
+        package="_global_",
+        groups="ocb_mods",
+    )
+    # Create a  partial configuration associated with the above function (for easy extensibility)
+
+    store.add_to_hydra_store(overwrite_ok=True)
+    steps_store.add_to_hydra_store(overwrite_ok=True)
+
+    # Create CLI endpoint
+    api_endpoint = hydra.main(config_name=name, version_base="1.3", config_path=".")(
+        zen_endpoint
+    )
+    return api_endpoint
