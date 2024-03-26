06a50aeb660e77ad12206ae63bfeccb032728156
Daniel Zhu <daniel.zhu@imt-atlantique.fr>
2024-03-26 11:34:03 +0100

    Update dvc pipeline

--- datachallenges/dc_ose_2021/dc_ose_2021_pipelines/compute_metrics.py
+++ datachallenges/dc_ose_2021/dc_ose_2021_pipelines/compute_metrics.py
@@ -20,7 +20,7 @@ stages = {
     "_02_filter_and_merge": pb(
         qf_filter_merge_daily_ssh_tracks.run,
         input_dir="${.._01_dl_tracks.download_dir}",
-        output_path="data/downloads/ref/${.._01_dl_tracks.sat}.nc",
+        output_path="data/prepared/ref/${.._01_dl_tracks.sat}.nc",
         min_lon="${...params.min_lon}",
         max_lon="${...params.max_lon}",
         min_lat="${...params.min_lat}",
@@ -33,7 +33,7 @@ stages = {
         grid_path="${...params.study_path}",
         grid_var="${...params.study_var}",
         track_path="${.._02_filter_and_merge.output_path}",
-        output_path="data/method_outputs/${...params.method}_on_track.nc",
+        output_path="data/prepared/method_outputs/${...params.method}_on_track.nc",
     ),
     "_04_1_lambdax": pb(
         alongtrack_lambdax.run,
@@ -41,7 +41,7 @@ stages = {
         study_path="${.._03_interp_on_track.output_path}",
         study_var="${.._03_interp_on_track.grid_var}",
         output_lambdax_path="data/metrics/lambdax_${...params.method}.json",
-        output_psd_path="data/metrics/psd_${...params.method}.nc",
+        output_psd_path="data/prepared/method_outputs/psd_${...params.method}.nc",
     ),
     "_04_2_mu": pb(
         dz_alongtrack_mu.run,
@@ -55,7 +55,7 @@ stages = {
 
 params = dict(
     method="default",
-    study_path="data/method_outputs/${.method}.nc",
+    study_path="data/downloads/method_outputs/${.method}.nc",
     study_var="ssh",
     sat='c2',
     min_time="2017-01-01", max_time="2017-12-31",
